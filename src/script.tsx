99560413514adc8b04aece8dc65714b6734fea5d
ameliapham <ptt.huong.8895@gmail.com>
2026-01-02 22:59:52 +0100

    Create linear color

--- src/script.tsx
+++ src/script.tsx
@@ -28,12 +28,13 @@ gui.close()
 const parameters = {
     count: 50000,
     size : 0.001,
-    color: 'pink',
     radius: 5,
     branches : 3,
     spin: 1,
     randomness: 0.3,
     randomnessPower: 2,
+    insideColor: '#ff6030',
+    outsideColor: '#1b3984',
 }
 
 let geometry: THREE.BufferGeometry | null = null
@@ -52,9 +53,15 @@ function generateGalaxy() {
     geometry = new THREE.BufferGeometry()
 
     const positions = new Float32Array(parameters.count * 3)
+    const colors = new Float32Array(parameters.count * 3)
+
+    const colorInside = new THREE.Color(parameters.insideColor)
+    const colorOutside = new THREE.Color(parameters.outsideColor)
+
     for (let i=0; i < parameters.count; i++) {
         const i3 = i * 3
-
+ 
+        // Position
         const radius = Math.random() * parameters.radius
         const spinAngle = radius * parameters.spin
         const branchAngle = (i % parameters.branches) / parameters.branches * Math.PI * 2 // Angle for each branch
@@ -63,19 +70,28 @@ function generateGalaxy() {
         const randomY = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : -1) * parameters.randomness * radius
         const randomZ = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : -1) * parameters.randomness * radius
 
-        positions[i3 + 0] = Math.cos(branchAngle + spinAngle) * radius + randomX
+        positions[i3    ] = Math.cos(branchAngle + spinAngle) * radius + randomX
         positions[i3 + 1] = 0 + randomY
         positions[i3 + 2] = Math.sin(branchAngle + spinAngle) * radius + randomZ
+    
+        // Color
+        const mixedColor = colorInside.clone()
+        mixedColor.lerp(colorOutside, radius / parameters.radius)
+
+        colors[i3    ] = mixedColor.r
+        colors[i3 + 1] = mixedColor.g
+        colors[i3 + 2] = mixedColor.b
     }
 
     geometry.setAttribute('position', new THREE.BufferAttribute(positions,3))
+    geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
 
     // Material
     material = new THREE.PointsMaterial({ 
         size: parameters.size, 
-        color: parameters.color,
         sizeAttenuation: true,
         blending : THREE.AdditiveBlending,
+        vertexColors: true, // Màu sẽ được lấy từ từng điểm, không phải từ material chung
     })
 
     // Points
@@ -86,12 +102,13 @@ generateGalaxy()
 
 gui.add(parameters, 'count').min(1000).max(100000).step(100).onFinishChange(generateGalaxy)
 gui.add(parameters, 'size').min(0.001).max(0.01).step(0.0001).onFinishChange(generateGalaxy)
-gui.addColor(parameters, 'color').onFinishChange(generateGalaxy)
 gui.add(parameters, 'radius').min(0.01).max(20).step(0.01).onFinishChange(generateGalaxy)
 gui.add(parameters, 'branches').min(2).max(20).step(1).onFinishChange(generateGalaxy)
 gui.add(parameters, 'spin').min(-5).max(5).step(0.001).onFinishChange(generateGalaxy)
 gui.add(parameters, 'randomness').min(0).max(2).step(0.001).onFinishChange(generateGalaxy)
 gui.add(parameters, 'randomnessPower').min(1).max(10).step(0.001).onFinishChange(generateGalaxy)
+gui.addColor(parameters, 'insideColor').onFinishChange(generateGalaxy)
+gui.addColor(parameters, 'outsideColor').onFinishChange(generateGalaxy)
 
 // --- Camera Setup ---
 const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight);
